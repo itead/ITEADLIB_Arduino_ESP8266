00d0639370909d726dc82e1889a97e44081b12dc
Chris Weiss <cweiss@gmail.com>
2014-11-14 09:39:00 -0600

    make begin non-blocking when card is not present

--- examples/udpSendReceivingString/udpSendReceivingString.ino
+++ examples/udpSendReceivingString/udpSendReceivingString.ino
@@ -60,7 +60,10 @@ WIFI wifi;
 void setup()
 {
   
-  wifi.begin();
+  if(!wifi.begin())
+  {
+  	DebugSerial.println("Begin error");
+  }
   bool b = wifi.Initialize(STA, SSID, PASSWORD);
   if(!b)
   {
